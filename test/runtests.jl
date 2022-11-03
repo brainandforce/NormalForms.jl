aa02cd2e24b789e86f1f90686bed4ab78f58b2c5
Brandon Flores <bsflores@wisc.edu>
2022-11-02 22:51:13 -0500

    Added a few very basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,11 @@ using Test
 
 @testset "NormalForms.jl" begin
     # Write your tests here.
+    @testset "Known square matrix" begin
+        M = [-2 1 1; 2 -1 1; 2 1 -1]
+        Fr = hnfr(M)
+        Fc = hnfc(M)
+        @test M * Fc.U == Fc.H
+        @test Fr.U * M == Fr.H
+    end
 end
