1e8b59b87ac61bc8c5084f8ce7b15a7035c311be
Brandon Flores <bsflores@wisc.edu>
2022-11-03 00:37:00 -0500

    Added tests for StaticArrays integration

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
-using NormalForms
 using Test
+using NormalForms
+using StaticArrays
 
 @testset "NormalForms.jl" begin
     # Write your tests here.
@@ -9,6 +10,14 @@ using Test
         Fc = hnfc(M)
         @test M * Fc.U == Fc.H
         @test Fr.U * M == Fr.H
+    end
+    @testset "SMatrix" begin
+        S = SMatrix{3,3,Int}([-2 1 1; 2 -1 1; 2 1 -1])
+        Fr = hnfr(S)
+        Fc = hnfc(S)
+        @test isbits(Fr)
+        @test S * Fc.U == Fc.H
+        @test Fr.U * S == Fr.H
     end
 end
 
