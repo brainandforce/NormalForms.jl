c48c2e81e4dfdb584cf3f5410cdb91503805d22a
Documenter.jl <documenter@juliadocs.github.io>
2022-11-03 04:57:28 +0000

    build based on aa02cd2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NormalForms","page":"Home","title":"NormalForms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This package provides functions for calculating the Hermite normal form (hnfr!(), hnfr(),  hnfc!(), hnfc()), and the Smith normal form (snf!(), snf!()), as well as factorization types RowHermite, ColumnHermite, and Smith, which are subtypes of LinearAlgebra.Factorization.","category":"page"}]
+}
