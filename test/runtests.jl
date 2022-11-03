e8c12d2c442fa907b57f0521d0ca3948a634ad2f
Brandon Flores <bsflores@wisc.edu>
2022-11-02 23:52:47 -0500

    Added matrix with known failed HNF calculation

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,10 @@ using Test
         @test Fr.U * M == Fr.H
     end
 end
+
+#= Known problem matrices:
+[ 9  7 -7  7 -3  7
+ -1  5  0  7 -4 -1
+  2  2  9  7 -5  3
+ -2  6  3 -9 -3  5] (hnfc fails - also in HermiteNormalForm.jl)
+=#
