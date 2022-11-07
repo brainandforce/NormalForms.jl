70f9937b21955f69fee376514c7f269e58351c22
Brandon Flores <bsflores@wisc.edu>
2022-11-07 11:12:07 -0600

    Added tests for Smith normal form

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,10 @@ using StaticArrays
         M = [-2 1 1; 2 -1 1; 2 1 -1]
         Fr = hnfr(M)
         Fc = hnfc(M)
+        S = snf(M)
         @test M * Fc.U == Fc.H
         @test Fr.U * M == Fr.H
+        @test S.U * M * S.V == S.S
     end
     @testset "SMatrix" begin
         S = SMatrix{3,3,Int}([-2 1 1; 2 -1 1; 2 1 -1])
@@ -26,4 +28,6 @@ end
  -1  5  0  7 -4 -1
   2  2  9  7 -5  3
  -2  6  3 -9 -3  5] (hnfc fails - also in HermiteNormalForm.jl)
+
+ Anything with [1 1; 0 1] seems to break snf()
 =#
