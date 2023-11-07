c3ad5d3165bb63fa873dbc5263d0c49fd06b6592
Brandon Flores <bsflores@wisc.edu>
2023-11-07 17:53:57 -0600

    Looped the zeroing algorithm

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -269,22 +269,25 @@ function zero_row_and_col!(
     k::Integer,
     ki::Integer = k
 )
-    # The cycle:
-    # Zero the row, reduce the column, zero the column, reduce the row
-    zero_row!(A, V, k, ki)
-    for n in axes(A, 1)[k+1:end]
-        mul = div(A[n,k], A[k,k], RoundToZero)
-        A[n,:] .-= mul .* A[k,:]
-        U[n,:] .-= mul .* U[k,:]
+    iterations = 0
+    # The cycle: Zero the row, reduce the column, zero the column, reduce the row
+    while !is_row_zero_after(A, k) || !is_col_zero_after(A, k)
+        iterations += 1
+        zero_row!(A, V, k, ki)
+        for n in axes(A, 1)[k+1:end]
+            mul = div(A[n,k], A[k,k], RoundToZero)
+            A[n,:] .-= mul .* A[k,:]
+            U[n,:] .-= mul .* U[k,:]
+        end
+        zero_col!(A, U, k, ki)
+        for n in axes(A, 2)[k+1:end]
+            mul = div(A[k,n], A[k,k], RoundToZero)
+            A[:,n] .-= mul .* A[:,k]
+            V[:,n] .-= mul .* V[:,k]
+        end
+        # TODO: what's a resonable bound on the number of iterations before we should error?
+        iterations > length(A) && error("Unreasonable amount of iterations (file an issue)")
     end
-    zero_col!(A, U, k, ki)
-    for n in axes(A, 2)[k+1:end]
-        mul = div(A[k,n], A[k,k], RoundToZero)
-        A[:,n] .-= mul .* A[:,k]
-        V[:,n] .-= mul .* V[:,k]
-    end
-    @assert is_col_zero_after(A, k)
-    @assert is_row_zero_after(A, k)
 end
 
 """
