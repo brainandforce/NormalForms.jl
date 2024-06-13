f9535bc406ab2edf2dd035f2aaa74f86bd993b5b
Brandon Flores <bsflores@wisc.edu>
2024-06-13 15:07:23 -0500

    Broadened type signatures to allow other `StaticMatrix` implemementations

--- src/staticarrays.jl
+++ src/staticarrays.jl
@@ -2,19 +2,19 @@ using .StaticArrays
 
 # Matrix appears slightly faster, but MMatrix allocates less memory
 # TODO: perhaps extend this for the sake of StaticArrays?
-detb(M::SMatrix) = detb!(convert(MMatrix, M))
+detb(M::StaticMatrix) = detb!(convert(MMatrix, M))
 
-function hnfc(M::SMatrix{D1,D2,<:Integer}, R::RoundingMode = NegativeOffDiagonal) where {D1,D2}
+function hnfc(M::StaticMatrix{D1,D2,<:Integer}, R::RoundingMode = NegativeOffDiagonal) where {D1,D2}
     (H, U, info) = hnf_ma!(MMatrix(M), R)
     return ColumnHermite(SMatrix{D1,D2}(H), SMatrix{D2,D2}(U), info)
 end
 
-function hnfr(M::SMatrix{D1,D2,<:Integer}, R::RoundingMode = NegativeOffDiagonal) where {D1,D2}
+function hnfr(M::StaticMatrix{D1,D2,<:Integer}, R::RoundingMode = NegativeOffDiagonal) where {D1,D2}
     (H, U, info) = hnf_ma!(MMatrix(M'), R)
     return RowHermite(SMatrix{D1,D2}(H'), SMatrix{D1,D1}(U'), info)
 end
 
-function snf(M::SMatrix{D1,D2,<:Integer}) where {D1,D2}
+function snf(M::StaticMatrix{D1,D2,<:Integer}) where {D1,D2}
     result = snf_ma!(MMatrix(M))
     return Smith(
         SMatrix{D1,D2}(result[1]),
